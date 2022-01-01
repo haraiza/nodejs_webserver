f9006d6e826b6bdc8be8dc8f94418470dc6a238b
humberto-desk <humberto_araiza@outlook.com>
2021-12-31 19:01:52 -0800

    cap 88

---
+++ app-old.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log(req);
+
+    res.write('Hola Mundo');
+    
+    res.end();
+})
+    .listen(8080);
+
+console.log('Escuchando el puerto', 8080);
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http.createServer((req, res) => {
-    res.write('Hola Mundo');
-    res.end();
+//Definicion de rutas
+//Home
+app.get('/', (req, res) => {
+    res.send('Hello World');
 })
-    .listen(8080);
 
-    console.log('Escuchando el puerto', 8080);
+//hola-mundo
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola-Mundo');
+})
+
+//Cualquier otra pagina que no este definida llegara aqui
+app.get('*', (req, res) => {
+    res.send('404 | Page not found');
+})
+
+//Esto aparecera en la consola
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+
+
